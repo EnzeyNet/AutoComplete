c14198e930054b2b7c207db3b7ed8e3964f22e51
Enzey <enzey@enzey.net>
2014-06-10 19:19:00 -0500

    Checkin copy of AntoComplete

---
+++ dist/AutoComplete.js
@@ -0,0 +1,192 @@
+(function(angular) {
+	var module = angular.module('net.enzey.autocomplete', []);
+
+	module.directive('nzAutoComplete', function($compile, $parse, $timeout) {
+		return {
+			scope: {},
+			restrict: 'AE',
+			link: function (scope, element, attr) {
+				var positionHintsFn = function(hintList, inputElem) {
+					var scroller = hintList.find('div')[0];
+					if (scroller.scrollHeight > scroller.clientHeight) {
+						angular.element(scroller).css('overflow-y', 'scroll');
+					} else {
+						angular.element(scroller).css('overflow-y', '');
+					}
+				};
+				/*
+				positionHintsFn = function(hintList, inputElem) {
+					$(hintList).position({
+						my: "left top",
+						at: "left bottom",
+						of: inputElem,
+						collision: 'flip'
+					});
+				}
+				*/
+				var displayHint = false;
+				var getResultsFn = $parse(attr.getResultsFn)(scope.$parent);
+				if (!getResultsFn || !typeof getResultsFn === 'function') {
+					throw 'A function that returns results is required!';
+				}
+				var isSelectionRequired = false;
+				if ( (angular.isDefined(attr.selectionRequired) && attr.selectionRequired === 'true') || 
+						angular.isDefined(attr.displayPath) ) {
+					isSelectionRequired = true;
+				}
+				element.addClass('inputHint');
+				var hintInputElem = $compile('<input class="hintBox" tabindex="-1"></input>')(scope);
+                var inputElem     = $compile('<input class="textEntry" ng-model="actualText"></input>')(scope);
+
+				var getHintDisplay = function() {
+					var hintDisplayObj = scope.hintables[scope.hintableIndex];
+					if (angular.isDefined(attr.displayPath)) {
+						return $parse(attr.displayPath)(hintDisplayObj);
+					}
+					return hintDisplayObj;
+				};
+
+				element.append(hintInputElem);
+				element.append($compile('<iframe></iframe>')(scope));
+				element.append(inputElem);
+
+				var selectRow = function(index, skipApply) {
+					if (index === scope.hintableIndex) {return;}
+
+					if (0 <= index && index < scope.hintables.length) {
+						var scroller = hintList.find('div')[0];
+						var hints =   angular.element(scroller).children();
+						var newHint = angular.element(hints[index]);
+
+						if (newHint[0].offsetTop < scroller.scrollTop) {
+							// scrollUp
+							scroller.scrollTop = newHint[0].offsetTop - 1;
+						} else if (newHint[0].offsetTop + newHint[0].clientHeight > scroller.scrollTop + scroller.clientHeight) {
+							// scrollDown
+							scroller.scrollTop = newHint[0].offsetTop + newHint[0].clientHeight - scroller.clientHeight + 1;
+						}
+
+						scope.hintableIndex = index;
+						if (!skipApply) {
+							scope.$apply();
+						}
+						if (displayHint === true) {
+							hintInputElem.val(getHintDisplay());
+						}
+					}
+				};
+
+                scope.select = function(selectedIndex) {
+                    scope.hintableIndex = selectedIndex;
+					scope.actualText = getHintDisplay();
+                    inputElem[0].focus();
+                };
+
+                scope.hoverOver = function(selectedIndex) {
+					selectRow(selectedIndex, true);
+                };
+
+				var hintText = angular.isDefined(attr.displayPath) ? 'hint.' + attr.displayPath : 'hint';
+                var hintList = $compile('<div class="scrollerContainer" ng-hide="hintables.length < 2"><iframe></iframe><div class="scroller"><div class="hint" ng-repeat="hint in hintables" ng-click="select($index)" ng-mouseover="hoverOver($index)" ng-class="{selectedHint: $index === hintableIndex}"><div>{{' + hintText + '}}</div></div></div></div>')(scope);
+                element.append(hintList);
+
+				scope.actualText = '';
+
+				inputElem.bind("focus", function() {
+					if (positionHintsFn && scope.hintables) {
+						$timeout(function() {
+							positionHintsFn(hintList, inputElem);
+						}, 1, false);
+					}
+				});
+				scope.$watch('actualText', function() {
+					displayHint = true;
+                    scope.hintableIndex = null;
+					hintInputElem.val('');
+					scope.hintables = [];
+
+					getResultsFn(scope.actualText).then(function(hintResults) {
+						scope.hintables = hintResults;
+						if (!scope.hintables) {scope.hintables = [];}
+
+						if (scope.hintables.length > 0) {
+							var regex = new RegExp('^' + scope.actualText);
+							var objParser = null;
+							if (angular.isDefined(attr.displayPath)) {
+								objParser = $parse(attr.displayPath);
+							}
+							scope.hintables.forEach(function(hintObj) {
+								if (objParser) {
+									displayHint = displayHint && regex.test(objParser(hintObj));
+								} else {
+									displayHint = displayHint && regex.test(hintObj);
+								}
+							});
+							$timeout(function() {
+								selectRow(0);
+							}, 0, false);
+
+						}
+
+						if (positionHintsFn) {
+							$timeout(function() {
+								positionHintsFn(hintList, inputElem);
+							}, 1, false);
+						}
+
+						setParentModel();
+					});
+				});
+
+				var setParentModel = function() {
+					if (isSelectionRequired) {
+						var selectedObj = scope.hintables[scope.hintableIndex];
+						if (scope.hintables && scope.hintables.length > 0 && 
+						( scope.actualText === selectedObj || scope.actualText === $parse(attr.displayPath)(selectedObj) ) ) {
+							$parse(attr.ngModel).assign(scope.$parent, selectedObj);
+						} else {
+							$parse(attr.ngModel).assign(scope.$parent);
+						}
+					} else {
+						$parse(attr.ngModel).assign(scope.$parent, scope.actualText);
+					}
+				};
+
+				inputElem.bind("keydown", function(e) {
+					if (scope.hintableIndex !== null && (e.keyCode === 13 || e.keyCode === 39)) {
+						scope.actualText = getHintDisplay();
+						scope.$apply();
+					} else if (e.keyCode === 40) {
+						// key down
+						if (scope.hintableIndex !== null && scope.hintables.length > 1) {
+							var newIndex;
+							if (scope.hintableIndex === scope.hintables.length - 1) {
+								newIndex = 0;
+							} else {
+								newIndex = scope.hintableIndex + 1;
+							}
+							selectRow(newIndex);
+						}
+                        e.preventDefault();
+                        e.stopPropagation();
+					} else if (e.keyCode === 38) {
+						// key up
+						if (scope.hintableIndex !== null && scope.hintables.length > 1) {
+							var newIndex;
+							if (scope.hintableIndex === 0) {
+								newIndex = scope.hintables.length - 1;
+							} else {
+								newIndex = scope.hintableIndex - 1;
+							}
+							selectRow(newIndex);
+						}
+                        e.preventDefault();
+                        e.stopPropagation();
+					}
+				});
+
+			}
+		};
+	});
+
+})(angular);
---
+++ example.js
@@ -0,0 +1,127 @@
+(function(angular) {
+
+	var module = angular.module('net.enzey.examples',
+		[
+			'net.enzey.autocomplete',
+		]
+	);
+
+	var getPerms = function(datums) {
+		var permArr = [];
+		var usedChars = [];
+
+		function permute(input) {
+			var i, ch;
+			for (i = 0; i < input.length; i++) {
+				ch = input.splice(i, 1)[0];
+				usedChars.push(ch);
+				if (input.length == 0) {
+					permArr.push(usedChars.slice());
+				}
+				permute(input);
+				input.splice(i, 0, ch);
+				usedChars.pop();
+			}
+			return permArr
+		};
+
+		return permute(datums);
+	};
+
+	module.controller('autoCompeteExample', function($scope, $timeout, $rootScope, $q) {
+		var getWatchCount = function (scope, scopeHash) {
+			// default for scopeHash
+			if (scopeHash === undefined) {
+				scopeHash = {};
+			}
+
+			// make sure scope is defined and we haven't already processed this scope
+			if (!scope || scopeHash[scope.$id] !== undefined) {
+				return 0;
+			}
+
+			var watchCount = 0;
+
+			if (scope.$$watchers) {
+				watchCount = scope.$$watchers.length;
+			}
+			scopeHash[scope.$id] = watchCount;
+
+			// get the counts of children and sibling scopes
+			// we only need childHead and nextSibling (not childTail or prevSibling)
+			watchCount+= getWatchCount(scope.$$childHead, scopeHash);
+			watchCount+= getWatchCount(scope.$$nextSibling, scopeHash);
+
+			return watchCount;
+		};
+		var updateWatchCount;
+		updateWatchCount = function() {
+			$timeout(function() {
+				$scope.watchCount = getWatchCount($rootScope);
+				updateWatchCount();
+			}, 3000, true);
+		};
+		updateWatchCount();
+
+		var arrayOfStuff = ['fruit', 'fun', 'family', 'fudge', 'nonfrugal', 'nonliquid', 'nonunison', 'neckpiece', 'nonnitric', 'nastiness', 'novachord', 'nonsaline', 'nonchurch', 'narcotist', 'nucleolus', 'nonbodily', 'nonmucous', 'nondebtor', 'nursemaid', 'nepheline', 'nonsuccor', 'nebulated', 'norwegian', 'nachising'];
+		var arrayOfObjs = [];
+		arrayOfStuff.forEach(function(text) {
+			arrayOfObjs.push({foo: {bar: text}});
+		});
+		$scope.searchFunction = function (inputText) {
+			var deferredFn = $q.defer();
+			if (!inputText || inputText.length < 1) {
+				deferredFn.reject();
+				return deferredFn.promise;
+			}
+
+			var regex = new RegExp('^' + inputText);
+			var results = [];
+			arrayOfStuff.forEach(function(text) {
+				if (regex.test(text)) {results.push(text);}
+			});
+
+			deferredFn.resolve(results);
+			return deferredFn.promise;
+		};
+		$scope.searchFunctionObjs = function (inputText) {
+			var deferredFn = $q.defer();
+			if (!inputText || inputText.length < 1) {
+				deferredFn.reject();
+				return deferredFn.promise;
+			}
+
+			$timeout(function() {
+				var regex = new RegExp('^' + inputText);
+				var results = [];
+				arrayOfObjs.forEach(function(obj) {
+					if (regex.test(obj.foo.bar)) {results.push(obj);}
+				});
+
+				deferredFn.resolve(results);
+			}, 1000, false);
+
+			return deferredFn.promise;
+		}
+		$scope.searchFunctionAnyLoc = function (inputText) {
+			var deferredFn = $q.defer();
+			if (!inputText || inputText.length < 1) {
+				deferredFn.reject();
+				return deferredFn.promise;
+			}
+
+			var regex = new RegExp(inputText);
+			var results = [];
+			arrayOfObjs.forEach(function(obj) {
+				if (regex.test(obj.foo.bar)) {results.push(obj);}
+			});
+
+			deferredFn.resolve(results);
+			return deferredFn.promise;
+		}
+
+		var data = getPerms(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+
+	});
+
+})(angular);
